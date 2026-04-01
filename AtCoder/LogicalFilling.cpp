8ac9d017c81b8315e42ede14bcc8b1610276d853
WuPinLiang <plwu703@gmail.com>
2026-04-01 15:52:52 +0800

    Practicing

---
+++ AtCoder/LogicalFilling.cpp
@@ -0,0 +1,22 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("fast-math")
+#include <iostream>
+#include <vector>
+#define int long long
+
+using namespace std;
+
+inline void solve() {
+    int n, k;cin >> n >> k;
+    string s;cin >> s;
+    
+    return ;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    solve();
+    return 0;
+}
