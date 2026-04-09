1f91e4273a76f8557d4bb4e01619f1728baba6ba
WuPinLiang <plwu703@gmail.com>
2026-04-09 13:13:50 +0800

    Solving Div Game

--- AtCoder/DivGame.cpp
+++ AtCoder/DivGame.cpp
@@ -4,19 +4,38 @@
 
 using namespace std;
 
+inline int bs(int target) {
+    int left = 0, right = 50;
+    while(left <= right) {
+        int mid = (left + right) / 2;
+        int val = mid * (mid + 1) / 2;
+        if(val == target) {
+            return mid;
+        }else if(val > target) 
+            right = mid - 1;
+        else 
+            left = mid + 1;
+    }
+    // the while loop will terminate if and only if right < left ,
+    // so we return right as our answer;
+    return right ;
+}
+
 inline void solve() {
     int n; cin >> n;
-    int mx = 0;
+    int ans = 0;
     for(int i = 2;i * i <= n;i++) {
-        if(n % i == 0) {
+        int cnt = 0;
+        while(n % i == 0) {
             n = n / i;
-            mx++;
+            cnt++;
         }
+        int mx = bs(cnt);
+        ans += mx;
     }
-    if(n > 1) {
-       mx++;
-    }
-    cout << mx << '\n';
+    if(n > 1)
+        ans++;
+    cout << ans << '\n';
     return ;
 }
 
